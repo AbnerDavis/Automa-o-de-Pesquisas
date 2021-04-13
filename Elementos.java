6b59e725e60bbe862043aaee7e73b08b91198e77
Abner Davis <binho.castro2.0@gmail.com>
2021-04-13 16:28:14 -0300

    PACKAGE - elementos

---
+++ Elementos.java
@@ -0,0 +1,19 @@
+package elementos;
+
+import org.openqa.selenium.By;
+
+public class Elementos {
+
+	// ELEMENTOS WEB
+
+	private By pesquisar = By.name("q");
+	
+	
+	// ELEMENTOS PUBLICOS DOS ELEMENTOS
+
+	public By getPesquisar() {
+		return pesquisar;
+
+	}
+
+}
